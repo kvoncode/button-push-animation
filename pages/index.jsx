fc651fb1783cb1720ead289ba3bd733a79fb28e3
kvoncode <kvoncode@gmail.com>
2020-01-17 10:03:30 +0900

    add: styled-components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,14 @@
+import styled from "styled-components";
+
+const StyledMain = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
+
 const Main = () => {
-  return <div>Hello</div>;
+  return <StyledMain>Hello</StyledMain>;
 };
 
 export default Main;
